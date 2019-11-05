38e6ec0cc14bc1b18da017858bc2f5a309343738
Schu5ter <christofschuster80@gmail.com>
2019-11-05 13:02:44 +0100

    Added intent on button Click and Activity change

--- app/src/main/java/com/example/stockit/MainActivity.java
+++ app/src/main/java/com/example/stockit/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.stockit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toSignUp (View view ){
+        Intent intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+
+    }
+
+
+    
+
+
 }
+
---
+++ app/src/main/java/com/example/stockit/SignUp.java
@@ -0,0 +1,20 @@
+package com.example.stockit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        Intent intent = getIntent();
+
+    }
+
+}
+
