f33d6860ff90eedf02e63446ea63d30720d14e46
Schu5ter <christofschuster80@gmail.com>
2019-11-27 16:33:05 +0100

    Firebase connection und Google Authentification

--- app/src/main/java/com/example/stockit/GoogleSignInActivity.java
+++ app/src/main/java/com/example/stockit/GoogleSignInActivity.java
@@ -1,49 +1,23 @@
-
-
 package com.example.stockit;
 
-
-
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.util.Log;
-
 import android.view.View;
-
 import android.widget.TextView;
-
-
-
 import androidx.annotation.NonNull;
-
-
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-
 import com.google.android.gms.common.api.ApiException;
-
 import com.google.android.gms.tasks.OnCompleteListener;
-
 import com.google.android.gms.tasks.Task;
 
-
-
 import com.google.firebase.auth.AuthCredential;
-
 import com.google.firebase.auth.AuthResult;
-
 import com.google.firebase.auth.FirebaseAuth;
-
 import com.google.firebase.auth.FirebaseUser;
-
 import com.google.firebase.auth.GoogleAuthProvider;
 
 
@@ -53,124 +27,76 @@ public class GoogleSignInActivity extends BaseActivity implements
 
         View.OnClickListener {
 
-
-
     private static final String TAG = "GoogleActivity";
-
     private static final int RC_SIGN_IN = 9001;
 
-
-
     // [START declare_auth]
-
     private FirebaseAuth mAuth;
-
     // [END declare_auth]
 
 
 
     private GoogleSignInClient mGoogleSignInClient;
-
     private TextView mStatusTextView;
-
-    private TextView mDetailTextView;
-
+   // private TextView mDetailTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_firebase_auth);
-
         Intent intent = getIntent();
-
-
         // Views
 
         mStatusTextView = findViewById(R.id.status);
-
-        mDetailTextView = findViewById(R.id.detail);
-
-
+       // mDetailTextView = findViewById(R.id.detail);
 
         // Button listeners
 
         findViewById(R.id.signInButton).setOnClickListener(this);
-
         findViewById(R.id.signOutButton).setOnClickListener(this);
-
-        findViewById(R.id.disconnectButton).setOnClickListener(this);
+        findViewById(R.id.weiterbutton).setOnClickListener(this);
 
 
 
         // [START config_signin]
-
-        // Configure Google Sign In
-
+        //Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-
                 .requestIdToken(getString(R.string.default_web_client_id))
-
                 .requestEmail()
-
                 .build();
-
         // [END config_signin]
-
-
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-
-
         // [START initialize_auth]
-
         // Initialize Firebase Auth
-
         mAuth = FirebaseAuth.getInstance();
-
         // [END initialize_auth]
 
     }
 
-
-
     // [START on_start_check_user]
 
     @Override
-
     public void onStart() {
-
         super.onStart();
-
         // Check if user is signed in (non-null) and update UI accordingly.
-
         FirebaseUser currentUser = mAuth.getCurrentUser();
-
         updateUI(currentUser);
-
     }
 
     // [END on_start_check_user]
 
-
-
     // [START onactivityresult]
 
     @Override
-
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
-
-
-
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
 
         if (requestCode == RC_SIGN_IN) {
-
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
             try {
@@ -200,15 +126,10 @@ public class GoogleSignInActivity extends BaseActivity implements
     }
 
     // [END onactivityresult]
-
-
-
     // [START auth_with_google]
-
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
 
         Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
         // [START_EXCLUDE silent]
 
         showProgressDialog();
@@ -247,52 +168,33 @@ public class GoogleSignInActivity extends BaseActivity implements
 
                         }
 
-
-
                         // [START_EXCLUDE]
-
                         hideProgressDialog();
-
                         // [END_EXCLUDE]
 
                     }
-
                 });
-
     }
 
     // [END auth_with_google]
 
-
-
     // [START signin]
 
     private void signIn() {
-
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-
         startActivityForResult(signInIntent, RC_SIGN_IN);
-
     }
 
     // [END signin]
 
-
-
     private void signOut() {
 
         // Firebase sign out
-
         mAuth.signOut();
 
-
-
         // Google sign out
-
         mGoogleSignInClient.signOut().addOnCompleteListener(this,
-
                 new OnCompleteListener<Void>() {
-
                     @Override
 
                     public void onComplete(@NonNull Task<Void> task) {
@@ -310,67 +212,45 @@ public class GoogleSignInActivity extends BaseActivity implements
     private void revokeAccess() {
 
         // Firebase sign out
-
         mAuth.signOut();
 
-
-
         // Google revoke access
-
         mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
 
                 new OnCompleteListener<Void>() {
 
                     @Override
-
                     public void onComplete(@NonNull Task<Void> task) {
-
                         updateUI(null);
 
                     }
-
                 });
-
     }
 
 
 
     private void updateUI(FirebaseUser user) {
-
         hideProgressDialog();
 
         if (user != null) {
 
             mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-
-            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-
-
+          //  mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
 
             findViewById(R.id.signInButton).setVisibility(View.GONE);
-
             findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
 
         } else {
-
             mStatusTextView.setText(R.string.signed_out);
-
-            mDetailTextView.setText(null);
-
-
+         //   mDetailTextView.setText(null);
 
             findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
-
             findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
 
         }
-
     }
 
-
-
     @Override
-
     public void onClick(View v) {
 
         int i = v.getId();
@@ -383,7 +263,7 @@ public class GoogleSignInActivity extends BaseActivity implements
 
             signOut();
 
-        } else if (i == R.id.disconnectButton) {
+        } else if (i == R.id.weiterbutton) {
 
             revokeAccess();
 
