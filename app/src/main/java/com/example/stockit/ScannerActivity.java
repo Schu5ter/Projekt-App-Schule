404b364a9ea57e98ece51f3118456efa9ca90a85
n00d00er <manusnake@gmx.de>
2020-02-11 05:14:49 +0100

    Neuer Button

--- app/src/main/java/com/example/stockit/ScannerActivity.java
+++ app/src/main/java/com/example/stockit/ScannerActivity.java
@@ -154,6 +154,7 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
 
         if (result.getText() != "") {
             scannerView.stopCamera();
+
                    }
     }
 }
